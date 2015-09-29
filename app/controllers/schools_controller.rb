e37f557c1afe86b164a672d0d4c2a7dade88b256
Michelle Dotzenrod <dotzenrodm@gmail.com>
2015-09-28 19:37:15 -0500

    linked schools list schools/index

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -1,9 +1,14 @@
 class SchoolsController < ApplicationController
 
   def index
+    page  = (params[:page]  ||  0).to_i
+    schools = School.limit(25).offset(page * 25)
+    render locals: { schools: schools, page: page }
   end
 
   def show
+    school = School.find(params[:id])
+    render locals: { school: school }
   end
 
 
