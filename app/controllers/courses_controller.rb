f6230551d8f81b1e00cee9ac2a7006bcb4641e62
Michelle Dotzenrod <dotzenrodm@gmail.com>
2015-09-29 09:02:03 -0500

    views for courses

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,6 +1,9 @@
 class CoursesController < ApplicationController
 
   def index
+    page  = (params[:page]  ||  0).to_i
+    courses = Course.limit(25).offset(page * 25)
+    render locals: { courses: courses, page: page }
   end
 
   def show
